a973a985565fff016d9de8c7b80fbdbc0922f640
shivamrathore11 <shivamrathore@deloitte.com>
2023-02-03 10:11:22 +0530

    added sample code for eslint rules
    added sample code for eslint rules

--- src/aura/Person.js
+++ src/aura/Person.js
@@ -1,31 +1,30 @@
 var Person = function(first, last, middle) {
-    this.first = first;
-    this.middle = middle;
-    this.last = last;
-};
-
-Person.prototype = {
-
-    whoAreYou : function() {
-        return this.first + (this.middle ? ' ' + this.middle: '') + ' ' + this.last;
-    }
-
-};
-
-var a = NaN;
-
-if (a === NaN) {  // Noncompliant; always false
-  console.log("a is not a number");  // this is dead code
+    this.first = first;
+    this.middle = middle;
+    this.last = last;
+}; Person.prototype = {     whoAreYou : function() {
+        return this.first + (this.middle ? ' ' + this.middle: '') + ' ' + this.last;
+    } };
+p = {                     //non complaint
+    get name(){
+        // no returns.
+    }
+}; Object.defineProperty(p, "age", {   //non compliant
+    get: function (){
+        // no returns.
+    }
+}); var p = {                    //complaint
+    get name(){
+        return "nicholas";
+    }
+}; var a = NaN; if (a === NaN) {  // Noncompliant; always false
+  console.log("a is not a number");  // this is dead code
 }
 if (a !== NaN) { // Noncompliant; always true
-  console.log("a is not NaN"); // this statement is not necessarily true
-}
-
-
+  console.log("a is not NaN"); // this statement is not necessarily true
+} 
 for (var i = 0; i < strings.length; i--) {
-  console.log("dead code")
-}
-
-if (str == null && str.length == 0) {
-  console.log("String is empty");
+  console.log("dead code")
+} if (str == null && str.length == 0) {
+  console.log("String is empty");
 }
